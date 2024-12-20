ad1a66f457dd19c9889012ca4da5b253fa16de29
gurumdevv <gurumdevv@gmail.com>
2024-12-20 12:10:23 -0800

    feat: Appbar 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,36 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: HomePager(),
+    );
+  }
+}
+
+class HomePager extends StatelessWidget {
+  const HomePager({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(CupertinoIcons.camera, color: Colors.black),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(CupertinoIcons.paperplane, color: Colors.black),
+            onPressed: () {},
+          )
+        ],
+        title: Image.asset(
+          'assets/logo.png', //파일 경로를 작성
+          height: 32,
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+      ),
+      body: Center(child: Text("Hello HomePage")),
     );
   }
 }

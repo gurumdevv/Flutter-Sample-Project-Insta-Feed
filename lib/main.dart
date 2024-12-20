975d27f1750eabbab0b10ca38f0a73d9025b089e
gurumdevv <gurumdevv@gmail.com>
2024-12-20 12:22:02 -0800

    feat: 피드 화면 구현

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,71 @@ class HomePager extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.white,
       ),
-      body: Center(child: Text("Hello HomePage")),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start, //default: center
+        children: [
+          // 이미지
+          Image.network(
+            "https://i.ibb.co/YjjLCS7/cat.png",
+            height: 400,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.heart,
+                  color: Colors.black,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.chat_bubble,
+                  color: Colors.black,
+                ),
+              ),
+              Spacer(), //최대 크기까지 커지면서 공백을 만듬
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  CupertinoIcons.bookmark,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "2 likes",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "My cat is docile even when bathed. I put a duck on his head in the wick and he's staring at me. Isn't it so cute??",
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "FEBURARY 6",
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
